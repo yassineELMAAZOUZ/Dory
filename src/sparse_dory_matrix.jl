2bad00a8882e12e0a67745bad67bcca2e51197bf
Avi Kulkarni <avinash@mis.mpg.de>
2019-07-11 14:37:11 +0200

    Attached some utility functions.

---
+++ src/sparse_dory_matrix.jl
@@ -0,0 +1,34 @@
+
+@doc Markdown.doc"""
+    maximum(abs, A::SMat{fmpz}) -> fmpz
+  Finds the largest, in absolute value, entry of $A$. 
+  Note that `abs` must be defined for the entry type.
+"""
+function maximum(::typeof(abs), A::SMat{T} where T)
+    if length(A.rows) == 0
+        return zero(FlintZZ)
+    end
+    m = abs(A.rows[1].values[1])
+    for i in A.rows
+        for j in i.values
+            if cmpabs(m, j) < 0
+                m = j
+            end
+        end
+    end
+    return abs(m)
+end
+
+
+function size(A::SMat{T} where T, i::Int64)
+    return size(A)[i]
+end
+
+function rows(A::SMat{T} where T)
+    return A.rows
+end
+
+## Temporary patch.
+function Base.deepcopy(A::SMat{T} where T)
+    return Hecke.copy(A)
+end
